b4c7cd966e249edc28b5d4b623420660868fcbdd
JorgeFigueroa <jorgefigueroavindel@gmail.com>
2024-02-18 15:17:18 -0300

    huge commit, adding the code app, local develop made by devops for developer, new structured main.tf having more resources to handle high app ussage, and database model script

---
+++ infrastructure/README.md
@@ -0,0 +1,8 @@
+# ati-kiu
+AWS Terraform infrastructure for scalable apps
+
+Resources:
+*VPC                        (Managing the networking)
+*EKS Cluster                (Managing the app deploy and scaling)
+*Auror MySQL Database       (Managing the app data at HA)
+*ECR Repository             (Managing the container images for EKS deployments)
---
+++ infrastructure/terraform.tf
@@ -0,0 +1,41 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  cloud {
+    organization = "Home-DevOps"
+    hostname = "app.terraform.io"
+
+    workspaces {
+      project = "ati-kiu"
+      tags = ["ati-kiu"]
+    }
+  }
+
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.37.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
+
