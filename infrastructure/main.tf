bc425beb29fb3eb0d1c1231dcc318c809b191af8
JorgeFigueroa <jorgefigueroavindel@gmail.com>
2024-02-18 22:29:03 -0300

    adding parameter subnet group resource to fix terraform rds cluster creation issue, re apply and test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -92,6 +92,15 @@ module "vpc_endpoints" {
     tags = local.db_tags
 }
 
+resource "aws_db_subnet_group" "kiu-db-subnet-group" {
+  name       = "kiu-db-subnet-group"
+  subnet_ids = module.vpc.private_subnets
+
+  tags = {
+    Name = "kiu database subnet group"
+  }
+}
+
 ################################################################################
 # ECR Module
 ################################################################################
@@ -225,7 +234,7 @@ module "aurora" {
     }
 
     vpc_id               = module.vpc.vpc_id
-    db_subnet_group_name = module.vpc.subnet_ids
+    db_subnet_group_name = module.vpc.database_subnet_group_name
     security_group_rules = {
     vpc_ingress = {
         cidr_blocks = module.vpc.private_subnets_cidr_blocks
